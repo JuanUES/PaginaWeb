cc5b147111142f5ce7d6aef19d6bd9353204eb54
Carlos-trj809 <79940098+Carlos-trj809@users.noreply.github.com>
2021-06-01 11:29:21 -0600

    Departamento de educacion agregado el estilo

--- Sitio_Web_FMP/resources/views/Academicos/Departamentos/cienciasEducacion.blade.php
+++ Sitio_Web_FMP/resources/views/Academicos/Departamentos/cienciasEducacion.blade.php
@@ -7,11 +7,98 @@
         <div class="page-title-alt-bg color-top"></div>
         <div class="page-title-box color-boton py-2 rounded">
             <h2 class="page-title text-white">Facultad Multidisciplinaria Paracentral</h2>
-        </div> 
+        </div>         
         <div class="my-3"></div>
         <!-- end page title -->
+        <div class="card-box"> 
+            <div class="row">
+                <div class="col-xl-8 p-2">
+                    <div class="tab-content pt-0" id="v-pills-tabContent">
+                        <div class="tab-pane fade active show" id="v-pills-social2" role="tabpanel" aria-labelledby="v-pills-social-tab2">
+                            <h4 class="header-title">Licenciatura en Trabajo Social</h4>                            
+                            <p class="mb-1 font-weight-bold ">CODIGO:</p>
+                            <p class="text-muted font-13 text-justify">L10439</p>
+                            <p class="mb-1 font-weight-bold">DESCRIPCIÓN:</p>
+                            <p class="text-muted font-13 text-justify">
+                                La Licenciatura en Trabajo Social en la UES nace a iniciativa del personal docente de la Escuela de Trabajo Social y se concretiza por medio de un Convenio entre el Gobierno de la República de El Salvador a través del Ministerio de Educación y la Universidad de El Salvador, firmado el 20 de mayo de 1999.
+                            </p>  
+                            <p class="mb-1 font-weight-bold">OBJETIVO:</p>
+                            <p class="text-muted font-13 text-justify">
+                                Formar profesionales en Trabajo Social, con fundamentación humanista, teórica-técnica-metodológica con un marco axiológico sólido que contribuya al desarrollo de las potencialidades e iniciativas de las personas, grupos y sectores poblacionales que posibiliten la construcción de opciones y alternativas tendientes a la promoción y transformación de la realidad.
+                            </p> 
+                                            
+    
+                            <p class="mb-1 font-weight-bold">TIEMPO DE DURACIÓN:</p>
+                            <p class="text-muted font-13">
+                                5 años (10 ciclos).
+                            </p>
+    
+                            <p class="mb-1 font-weight-bold">GRADO Y TÍTULO QUE OTORGA:</p>
+                            <p class="text-muted font-13">
+                                LICENCIADO (A) EN TRABAJO SOCIAL.
+                            </p>
+                            <p class="mb-1 font-weight-bold">PENSUM:</p>
+                            <a href="#" type="submit" class="btn btn-outline-danger"> <div class="mdi mdi-file-pdf mdi-24px align-top">Descargar</div></a>
+                        </div>
+                        <div class="tab-pane fade" id="v-pills-profile2" role="tabpanel" aria-labelledby="v-pills-profile-tab2">
+                            <h4 class="header-title">Profesorado en Educación Básica para Primero y Segundo Ciclos</h4>                            
+                            <p class="mb-1 font-weight-bold ">CODIGO:</p>
+                            <p class="text-muted font-13 text-justify">P70402</p>
+                            <p class="mb-1 font-weight-bold">DESCRIPCIÓN:</p>
+                            <p class="text-muted font-13 text-justify">
+                                El Profesorado en Educación Básica para Primero y Segundo Ciclos está diseñado para formar maestros/as con capacidad científica, técnica, pedagógica y ética, para el ejercicio profesional de la docencia en el nivel de educación básica para primero y segundo ciclos, los cuales trabajarían con niños y niñas entre 7 y 12 años.
+                            </p> 
+                            <p class="text-muted font-13 text-justify">
+                                Esta especialidad se ofrece en seis ciclos, con una duración mínima de tres años, con un total de 23 asignaturas, en cuyo desarrollo gradual se contemplan tres componentes que se articulan simultáneamente desde el inicio hasta el final del plan de estudios.
+                            </p>
+                            <p class="text-muted font-13 text-justify">
+                                El primero de ellos tiene como propósito la formación general, común a todos los profesorados y se tratará en 9 cursos; un segundo componente está referido a la formación especializada, que centra su interés en el dominio de los contenidos curriculares y los conocimientos específicos de estrategias didácticas, necesario para su desempeño docente; este componente se desarrolla en 9 cursos; y un tercer componente que corresponde a la práctica docente, en la que se observará y reflexionará las situaciones reales de enseñanza-aprendizaje del nivel de educación básica para primero y segundo ciclo.
+                            </p>
+                                            
+                            <p class="mb-1 font-weight-bold">TIEMPO DE DURACIÓN:</p>
+                            <p class="text-muted font-13">
+                                3 años.
+                            </p>
+    
+                            <p class="mb-1 font-weight-bold">GRADO Y TÍTULO QUE OTORGA:</p>
+                            <p class="text-muted font-13">
+                                PROFESOR (A) EN EDUCACIÓN BÁSICA PARA PRIMERO Y SEGUNDO CICLOS.
+                            </p>
+                            <p class="mb-1 font-weight-bold">PENSUM:</p>
+                            <a href="#" type="submit" class="btn btn-info"> <div class="mdi mdi-file-pdf mdi-24px align-top">Descargar</div></a>
+                        </div>
+                        <div class="tab-pane fade" id="v-pills-messages2" role="tabpanel" aria-labelledby="v-pills-messages-tab2">
+                            
+                        </div>
+                        <div class="tab-pane fade" id="v-pills-settings2" role="tabpanel" aria-labelledby="v-pills-settings-tab2">
+                        
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+                <div class="col-xl-4">
+                    <h4>Departamentos de Ciencia de la Educación</h4>
+                    <div class="nav flex-column nav-pills nav-pills-tab" id="v-pills-tab2" role="tablist" aria-orientation="vertical">
+                        <a class="nav-link active show mb-2 border" id="v-pills-social-tab2" data-toggle="pill" href="#v-pills-social2" role="tab" aria-controls="v-pills-social2"
+                            aria-selected="true">
+                            Licenciatura en Trabajo Social</a>
+                        <a class="nav-link mb-2 border" id="v-pills-profile-tab2" data-toggle="pill" href="#v-pills-profile2" role="tab" aria-controls="v-pills-profile2"
+                            aria-selected="false">
+                            Profesorado en Educación Básica para Primero y Segundo Ciclos</a>
+                        <a class="nav-link mb-2 border" id="v-pills-messages-tab2" data-toggle="pill" href="#v-pills-messages2" role="tab" aria-controls="v-pills-messages2"
+                            aria-selected="false">
+                            Profesorado en Matemática para Tercer Ciclo de Educación Básica y Educación Media </a>
+                        <a class="nav-link mb-2 border" id="v-pills-settings-tab2" data-toggle="pill" href="#v-pills-settings2" role="tab" aria-controls="v-pills-settings2"
+                            aria-selected="false">
+                            Construyendo</a>
+                        <a class="nav-link mb-2 border" id="v-pills-settings-tab2" data-toggle="pill" href="#v-pills-settings2" role="tab" aria-controls="v-pills-settings2"
+                            aria-selected="false">
+                            Construyendo</a>
+                    </div>
+                </div> <!-- end col -->
+            </div> <!-- end row--> 
+        </div> <!-- end card-box -->
 
-        
-    <div>
-<div>
+    </div> <!-- end container-->
+</div>
+<!-- end row -->
 @endsection
