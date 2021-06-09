51400fab386cb5258d538f115340d6887e953d42
jeffeth <ra12081@ues.edu.sv>
2021-06-09 14:57:33 -0600

    Se corrigio horrores ortograficos de trejo en mision y vision, se cambio de js en libreria de editor

--- Sitio_Web_FMP/resources/views/Nosotros/misionVision.blade.php
+++ Sitio_Web_FMP/resources/views/Nosotros/misionVision.blade.php
@@ -15,13 +15,13 @@
         <div class="row">
             <div class="col-xl-6">
                 <div class="card-box" height="800"> 
-                    <h3 class="text-center">Mision</h3>
+                    <h3 class="text-center">Misión</h3>
                     <p class="text-justify">Ser líder en el campo de la educación superior universitaria; reconocida por la calidad humana, científica y técnica; promotora de valores éticos; con infraestructura y equipo adecuado; un presupuesto que garantice el funcionamiento eficiente y una oferta académica pertinente para el desarrollo humano integral de la Región Paracentral y de la nación.</p>
                 </div> <!-- end card-box -->
             </div><!-- end col -->      
             <div class="col-xl-6" >
                 <div class="card-box container-flui"> 
-                    <h3 class="text-center">Vision</h3>
+                    <h3 class="text-center">Visión</h3>
                     <p class="text-justify" >Somos una institución de educación superior de carácter público; formadora de profesionales de grado y postgrado en diferentes áreas del saber; generadora de conocimientos científicos, tecnológicos y humanistas; comprometida con el desarrollo humano sostenible en la Región Paracentral de El Salvador.</p>
                 </div> <!-- end card-box -->
             </div><!-- end col -->      
