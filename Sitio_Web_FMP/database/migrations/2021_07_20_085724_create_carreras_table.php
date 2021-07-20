34f9ccbf26ed910158d71adccaad5e96bd5cead6
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-07-20 09:34:37 -0600

    migraciones

---
+++ Sitio_Web_FMP/database/migrations/2021_07_20_085724_create_carreras_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carreras', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo_carrera');
+            $table->string('nombre_carrera');
+            $table->string('modalidad_carrera');
+            $table->bigInteger('id_depto');
+            $table->foreign('id_depto')
+            ->references('id')
+            ->on('departamentos')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carreras');
+    }
+}
