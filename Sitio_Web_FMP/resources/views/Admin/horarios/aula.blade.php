c198dd278d651b063641127fea177f89411d3f82
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-07-26 13:56:46 -0600

    cambios en blade aulas

--- Sitio_Web_FMP/resources/views/Admin/horarios/aula.blade.php
+++ Sitio_Web_FMP/resources/views/Admin/horarios/aula.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.admin')
 
-@section('plugins')
-<link href="{{ asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" /> 
-@endsection
-@section('plugins-js')
-   <!--Librerias js para datatable
-    <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('js/index/index.datatable.js') }}"></script>-->
-    <script src="{{ asset('template-admin/dist/assets/js/licencia/httpLicencia.min.js') }}"></script>
-@endsection
-
 @section('content')
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -99,80 +88,111 @@
                  <button type="button" style="margin-left: 450px;" class="btn btn-primary dripicons-plus" data-toggle="modal" data-target="#exampleModalCenter"></button>
                 </div>
             </div>
-            <div class="responsive-table-plugin">
-                <div class="table-rep-plugin">
-                    <div class="table table-borderless  btn-table table-sm table-responsive-md" data-pattern="priority-columns">
-                        <table id="tech-companies-1" class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th data-priority="1">Código</th>
-                                <th data-priority="3">Nombre</th>
-                                <th data-priority="1">Ubicación</th>
-                                <th data-priority="3">Capacidad</th>
-                                <th data-priority="3">Acciones</th>
-                              
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th>GOOG <span class="co-name">Google Inc.</span></th>
-                                <td>597.74</td>
-                                <td>12:12PM</td>
-                                <td>14.81 (2.54%)</td>
-                                <td>582.93</td>
-                           
-                            </tr>
-                            <tr>
-                                <th>AAPL <span class="co-name">Apple Inc.</span></th>
-                                <td>378.94</td>
-                                <td>12:22PM</td>
-                                <td>5.74 (1.54%)</td>
-                                <td>373.20</td>
-                             
-                            </tr>
-                            <tr>
-                                <th>AMZN <span class="co-name">Amazon.com Inc.</span></th>
-                                <td>191.55</td>
-                                <td>12:23PM</td>
-                                <td>3.16 (1.68%)</td>
-                                <td>188.39</td>
-                             
-                            </tr>
-                            <tr>
-                                <th>ORCL <span class="co-name">Oracle Corporation</span></th>
-                                <td>31.15</td>
-                                <td>12:44PM</td>
-                                <td>1.41 (4.72%)</td>
-                                <td>29.74</td>
-                              
-                            </tr>
-                            <tr>
-                                <th>MSFT <span class="co-name">Microsoft Corporation</span></th>
-                                <td>25.50</td>
-                                <td>12:27PM</td>
-                                <td>0.66 (2.67%)</td>
-                                <td>24.84</td>
-                            </tr>
-                            
-                            <tr>
-                                <th>YHOO <span class="co-name">Yahoo! Inc.</span></th>
-                                <td>15.81</td>
-                                <td>12:25PM</td>
-                                <td>0.11 (0.67%)</td>
-                                <td>15.70</td>
-                            </tr>
-                            
-                            
-                            
-                            </tbody>
-                        </table>
-                    </div> <!-- end table-responsive-->
-
-                </div> <!-- table-rep-plugin-->
-            </div>
+            <table  class="table table-sm" id="table-aulas">
+                <thead>
+                <tr>
+                    <th data-priority="1">Código</th>
+                    <th data-priority="3">Nombre</th>
+                    <th data-priority="1">Ubicación</th>
+                    <th data-priority="3">Capacidad</th>
+                    <th data-priority="3">Acciones</th>
+                  
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th>GOOG <span class="co-name">Google Inc.</span></th>
+                    <td>597.74</td>
+                    <td>12:12PM</td>
+                    <td>14.81 (2.54%)</td>
+                    <td>582.93</td>
+               
+                </tr>
+                <tr>
+                    <th>AAPL <span class="co-name">Apple Inc.</span></th>
+                    <td>378.94</td>
+                    <td>12:22PM</td>
+                    <td>5.74 (1.54%)</td>
+                    <td>373.20</td>
+                 
+                </tr>
+                <tr>
+                    <th>AMZN <span class="co-name">Amazon.com Inc.</span></th>
+                    <td>191.55</td>
+                    <td>12:23PM</td>
+                    <td>3.16 (1.68%)</td>
+                    <td>188.39</td>
+                 
+                </tr>
+                <tr>
+                    <th>ORCL <span class="co-name">Oracle Corporation</span></th>
+                    <td>31.15</td>
+                    <td>12:44PM</td>
+                    <td>1.41 (4.72%)</td>
+                    <td>29.74</td>
+                  
+                </tr>
+                <tr>
+                    <th>MSFT <span class="co-name">Microsoft Corporation</span></th>
+                    <td>25.50</td>
+                    <td>12:27PM</td>
+                    <td>0.66 (2.67%)</td>
+                    <td>24.84</td>
+                </tr>
+                
+                <tr>
+                    <th>YHOO <span class="co-name">Yahoo! Inc.</span></th>
+                    <td>15.81</td>
+                    <td>12:25PM</td>
+                    <td>0.11 (0.67%)</td>
+                    <td>15.70</td>
+                </tr>
+                
+                
+                
+                </tbody>
+            </table>
 
         </div> <!-- end card-box -->
     </div> <!-- end col -->
 </div>
 <!-- end row -->   
+@endsection
+
+@section('plugins-js')
+<!-- Dashboard Init JS -->
+<script src="{{ asset('template-admin/dist/assets/js/pages/dashboard.init.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('#table-aulas').DataTable({
+          "language": {
+              "decimal":        ".",
+              "emptyTable":     "No hay datos para mostrar",
+              "info":           "Del _START_ al _END_ (_TOTAL_ total)",
+              "infoEmpty":      "Del 0 al 0 (0 total)",
+              "infoFiltered":   "(Filtrado de todas las _MAX_ entradas)",
+              "infoPostFix":    "",
+              "thousands":      "'",
+              "lengthMenu":     "Mostrar _MENU_ entradas",
+              "loadingRecords": "Cargando...",
+              "processing":     "Procesando...",
+              "search":         "Buscar:",
+              "zeroRecords":    "No hay resultados",
+              "paginate": {
+                "first":      "Primero",
+                "last":       "Último",
+                "next":       "Siguiente",
+                "previous":   "Anterior"
+              },
+              "aria": {
+                "sortAscending":  ": Ordenar de manera Ascendente",
+                "sortDescending": ": Ordenar de manera Descendente ",
+              }
+            },
+              "pagingType": "full_numbers",
+              "lengthMenu":		[[5, 10, 20, 25, 50, -1], [5, 10, 20, 25, 50, "Todos"]],
+		        	"iDisplayLength":	5,
+        });  
+      });
+</script>
 @endsection
