030a6f59afb3cda9ba5bc9276590b8f7b29ecff1
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-08-23 11:03:30 -0600

    se completo y corrigio lo de contenido (editor de texto)

--- Sitio_Web_FMP/resources/views/Academicos/cienciasAgronomicas.blade.php
+++ Sitio_Web_FMP/resources/views/Academicos/cienciasAgronomicas.blade.php
@@ -72,12 +72,52 @@
                                         Ingeniero(a) Agrónomo.
                                     </p>
                                 --> 
-                                <div class="col-xl-12">     
-                                    <div class="form-group">                                               
-                                        <label for="contenido">Contenido <code>*</code></label>
-                                        <textarea value="" class="form-control summernote-config" name="contenido" id="contenido"></textarea>
-                                    </div>
-                                </div>
+                                @auth
+<!-- Esto va en el content-->
+<?php
+    $variableNoTocar = 'localizacion';
+    $localizacion ='ingenieriaAgronomica';
+    $contenido = App\Models\Pagina\ContenidoHtml::where($variableNoTocar,$localizacion)->first();
+
+?>
+    <div class="col-xl-12">
+        <form action="{{ route('contenido', ['localizacion'=>$localizacion]) }}" method="POST"  
+            class="parsley-examples"  id="contenido{{$localizacion}}">
+            @csrf
+            <div class="alert alert-primary text-white py-1" 
+                    role="alert" style="display:none" id="notificacion{{$localizacion}}">                                               
+            </div>
+            <div class="row">
+                <div class="col-xl-12">   
+                    <div class="form-group">                       
+                        <textarea value="" class="form-control summernote-config" name="contenido"  rows="10">
+                            @if ($contenido!=null)
+                                {{$contenido->contenido}}
+                            @endif
+                        </textarea>
+                    </div>
+                </div>
+                <div class="col-xl-12">
+                    <div class="form-group">
+                        <button type="button" class="btn btn-primary waves-effect waves-light btn-block" 
+                            onclick="submitForm('#contenido{{$localizacion}}','#notificacion{{$localizacion}}')">
+                            <i class="fa fa-save fa-5 ml-3"></i> Guardar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>  
+    
+@endauth 
+
+@guest  
+<div class="col-xl-12 py-2">
+@if ($contenido!=null)
+    {!!$contenido->contenido!!}
+@endif
+</div>      
+@endguest
                                     <p class="mb-1 font-weight-bold">Pensum:</p>
                                     <a href="{{$pdfs->where('file','ingAgro.pdf')->first()==null 
                                         ? '#':asset('files/pdfs/'.$pdfs[0]->localizacion.'/ingAgro.pdf')}}"
@@ -150,12 +190,55 @@
                                         Ingeniero(a) Agroindustrial
                                     </p>
                                 -->
-                                <div class="col-xl-12">     
-                                    <div class="form-group">                                               
-                                        <label for="contenido">Contenido <code>*</code></label>
-                                        <textarea value="" class="form-control summernote-config" name="contenido" id="contenido"></textarea>
-                                    </div>
-                                </div>
+                                <!-- Esto va en el content-->
+<?php
+$variableNoTocar = 'localizacion';
+$localizacion ='ingenieriaAgroIndustrial';
+$contenido = App\Models\Pagina\ContenidoHtml::where($variableNoTocar,$localizacion)->first();
+
+?>
+@auth
+    
+
+<div class="col-xl-12">
+    <form action="{{ route('contenido', ['localizacion'=>$localizacion]) }}" method="POST"  
+        class="parsley-examples"  id="contenido{{$localizacion}}">
+        @csrf
+        <div class="alert alert-primary text-white py-1" 
+                role="alert" style="display:none" id="notificacion{{$localizacion}}">                                               
+        </div>
+        <div class="row">
+            <div class="col-xl-12">   
+                <div class="form-group">                       
+                    <textarea value="" class="form-control summernote-config" name="contenido"  rows="10">
+                        @if ($contenido!=null)
+                            {{$contenido->contenido}}
+                        @endif
+                    </textarea>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="form-group">
+                    <button type="button" class="btn btn-primary waves-effect waves-light btn-block" 
+                        onclick="submitForm('#contenido{{$localizacion}}','#notificacion{{$localizacion}}')">
+                        <i class="fa fa-save fa-5 ml-3"></i> Guardar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>  
+
+@endauth 
+
+@guest  
+<div class="col-xl-12 py-2">
+@if ($contenido!=null)
+{!!$contenido->contenido!!}
+@endif
+</div>      
+@endguest
+                              
                                     <p class="mb-1 font-weight-bold">Pensum:</p>
                                     <a href="{{$pdfs->where('file','ingIndus.pdf')->first()==null 
                                         ? '#':asset('files/pdfs/'.$pdfs[0]->localizacion.'/ingIndus.pdf')}}"
--- Sitio_Web_FMP/resources/views/Academicos/proyeccionSocial.blade.php
+++ Sitio_Web_FMP/resources/views/Academicos/proyeccionSocial.blade.php
@@ -262,14 +262,53 @@
                                 
                             </ul>
                         -->
+                        <?php
+                        $variableNoTocar = 'localizacion';
+                        $localizacion ='lineamientos';
+                        $contenido = App\Models\Pagina\ContenidoHtml::where($variableNoTocar,$localizacion)->first();
+                        
+                        ?>
                         @auth
-                        <div class="col-xl-12">     
-                            <div class="form-group">                                               
-                                <label for="contenido">Contenido <code>*</code></label>
-                                <textarea value="" class="form-control summernote-config" name="contenido" id="contenido"></textarea>
-                            </div>
-                        </div>
-                        @endauth
+                            
+                        
+                        <div class="col-xl-12">
+                            <form action="{{ route('contenido', ['localizacion'=>$localizacion]) }}" method="POST"  
+                                class="parsley-examples"  id="contenido{{$localizacion}}">
+                                @csrf
+                                <div class="alert alert-primary text-white py-1" 
+                                        role="alert" style="display:none" id="notificacion{{$localizacion}}">                                               
+                                </div>
+                                <div class="row">
+                                    <div class="col-xl-12">   
+                                        <div class="form-group">                       
+                                            <textarea value="" class="form-control summernote-config" name="contenido"  rows="10">
+                                                @if ($contenido!=null)
+                                                    {{$contenido->contenido}}
+                                                @endif
+                                            </textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="form-group">
+                                            <button type="button" class="btn btn-primary waves-effect waves-light btn-block" 
+                                                onclick="submitForm('#contenido{{$localizacion}}','#notificacion{{$localizacion}}')">
+                                                <i class="fa fa-save fa-5 ml-3"></i> Guardar
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>  
+                        
+                        @endauth 
+                        
+                        @guest  
+                        <div class="col-xl-12 py-2">
+                        @if ($contenido!=null)
+                        {!!$contenido->contenido!!}
+                        @endif
+                        </div>      
+                        @endguest
                             @auth
                             <div id="modalEliminar2" class="modal fade bs-example-modal-center" tabindex="-1" 
                             role="dialog" aria-labelledby="myCenterModalLabel" aria-hidden="true" >
