ac5ddb97e104948da5027d65d31f89975e2a6ec0
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-08-30 10:43:10 -0600

    correccion de url U a distancia

--- Sitio_Web_FMP/resources/views/index.blade.php
+++ Sitio_Web_FMP/resources/views/index.blade.php
@@ -484,7 +484,7 @@ role="dialog" aria-labelledby="myCenterModalLabel" aria-hidden="true" >
                             <a href="https://campus.ues.edu.sv/" class="btn btn-danger btn-block mt-3 text-left"><i class="mdi mdi-earth font-18"></i> Campus Virtual</a>
                             <a href="https://eel.ues.edu.sv/" class="btn btn-danger btn-block mt-3 text-left"><i class="mdi mdi-earth font-18"></i> Expediente en Linea</a>                      
                             <a href="https://correo.ues.edu.sv/" class="btn btn-danger  btn-block mt-3 text-left"><i class=" mdi mdi-email font-18"></i> Correo Institucional</a>                           
-                            <a href="https://www.facebook.com/DistanciaFMP" class="btn btn-danger  btn-block mt-3 text-left"><i class="mdi mdi-facebook border rounded font-16"></i> Universidad en Linea / Sede Paracentral</a> 
+                            <a href="https://www.facebook.com/EnLineaFMP" class="btn btn-danger  btn-block mt-3 text-left"><i class="mdi mdi-facebook border rounded font-16"></i> Universidad en Linea / Sede Paracentral</a> 
                             <a href="https://www.facebook.com/celeues" target="_blank" class="btn btn-danger btn-block mt-3 text-left"><i class=" mdi mdi-facebook border rounded font-16"></i> CELEUES</a>
                         </div> <!-- end card-box-->                        
                     </div> <!-- end col-->        
