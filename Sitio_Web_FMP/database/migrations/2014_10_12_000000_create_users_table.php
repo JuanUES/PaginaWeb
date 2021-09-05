b4f5e9ca8ac3300bc4b3f18e1f41bc84b54249e2
Carlos-trj809 <79940098+Carlos-trj809@users.noreply.github.com>
2021-09-05 13:28:02 -0600

    agregar a migraciones el campo boolean a la tabla user

--- Sitio_Web_FMP/database/migrations/2014_10_12_000000_create_users_table.php
+++ Sitio_Web_FMP/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
+            $table->boolean('estado')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- Sitio_Web_FMP/database/seeders/UserSeeder.php
+++ Sitio_Web_FMP/database/seeders/UserSeeder.php
@@ -20,48 +20,57 @@ class UserSeeder extends Seeder
             'name'=>'Liseth Guadalupe Merino de Córdova',
             'email'=>'liseth.merino@ues.edu.sv',
             'password'=>Hash::make('12345678'),
+            'estado' => true,
         ]);
         
         $admin = User::create([
             'name' => 'Administrador',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
+            'estado' => true,
         ]);
         $presupuestario = User::create([
             'name' => 'Presupuestario',
             'email' => 'presupuestario@gmail.com',
             'password' => Hash::make('presupuestario'),
+            'estado' => true,
         ]);
         $decano = User::create([
             'name' => 'Decano',
             'email' => 'decano@gmail.com',
             'password' => Hash::make('decano'),
+            'estado' => true,
         ]);
         $secretario = User::create([
             'name' => 'Secretario',
             'email' => 'secretario@gmail.com',
             'password' => Hash::make('secretario'),
+            'estado' => true,
         ]);
         $user = User::create([
             'name' => 'Pagina Admin',
             'email' => 'Pagina@ues.edu.sv',
             'password' => Hash::make('Pagina'),
+            'estado' => true,
         ]);
         $user = User::create([
             'name'=>'Jefe Academico',
             'email'=> 'jefe@ues.edu.sv',
             'password'=>Hash::make('jefe'),
+            'estado' => true,
         ]);
 
         $rrhh = User::create([
             'name'=>'Recurso Humano',
             'email'=> 'rrhh@ues.edu.sv',
             'password'=>Hash::make('rrhh'),
+            'estado' => true,
         ]);
         
         //Asignar el role usuario
         $usu->assignRole('super-admin');
         $admin->assignRole('super-admin');
+        $user->assignRole('super-admin');
         $presupuestario->assignRole('Transparencia-Presupuestario');
         $decano->assignRole('Transparencia-Decano');
         $secretario->assignRole('Transparencia-Secretario');
