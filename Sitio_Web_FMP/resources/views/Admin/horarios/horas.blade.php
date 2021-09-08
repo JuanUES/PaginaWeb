af7d40fe3578cade8cc733a456f689d5dc82e503
SoporteUti <87437110+SoporteUti@users.noreply.github.com>
2021-09-08 15:54:14 -0600

    detalles data table

--- Sitio_Web_FMP/resources/views/Admin/horarios/horas.blade.php
+++ Sitio_Web_FMP/resources/views/Admin/horarios/horas.blade.php
@@ -122,7 +122,7 @@
                    data-target="#horas"></button>
                 </div>
             </div>
-            <table  class="table table-bordered">
+            <table  class="table table-bordered" style="width: 100%">
                 <thead>
                 <tr>
                     <th data-priority="1" style="width: 5%;">N°</th>
@@ -183,37 +183,5 @@
         editar($json);
         }
 </script>
-<script>
-    $(document).ready(function () {
-        $('.table').DataTable({
-          "language": {
-              "decimal":        ".",
-              "emptyTable":     "No hay datos para mostrar",
-              "info":           "Del _START_ al _END_ (_TOTAL_ total)",
-              "infoEmpty":      "Del 0 al 0 (0 total)",
-              "infoFiltered":   "(Filtrado de todas las _MAX_ entradas)",
-              "infoPostFix":    "",
-              "thousands":      "'",
-              "lengthMenu":     "Mostrar _MENU_ entradas",
-              "loadingRecords": "Cargando...",
-              "processing":     "Procesando...",
-              "search":         "Buscar:",
-              "zeroRecords":    "No hay resultados",
-              "paginate": {
-                "first":      "Primero",
-                "last":       "Último",
-                "next":       "Siguiente",
-                "previous":   "Anterior"
-              },
-              "aria": {
-                "sortAscending":  ": Ordenar de manera Ascendente",
-                "sortDescending": ": Ordenar de manera Descendente ",
-              }
-            },
-              "pagingType": "full_numbers",
-              "lengthMenu":		[[5, 10, 20, 25, 50, -1], [5, 10, 20, 25, 50, "Todos"]],
-		        	"iDisplayLength":	5,
-        });  
-      });
-</script>
+<script src="{{ asset('js/scripts/data-table.js') }}" defer></script>
 @endsection
