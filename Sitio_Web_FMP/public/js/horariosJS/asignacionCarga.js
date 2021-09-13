e3117eff8c469590170cecd805e2faa1c36fea7a
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-09-13 15:33:52 -0600

    cambios

--- Sitio_Web_FMP/public/js/horariosJS/asignacionCarga.js
+++ Sitio_Web_FMP/public/js/horariosJS/asignacionCarga.js
@@ -1,15 +1,26 @@
 $('#A_carga').on('change', function() {
     let option = document.getElementById("A_carga").value;
+    if(option=='ad'){
+        document.getElementById("cantidad").disabled=true;
+        document.getElementById("dias").disabled=false;
+    }else if(option=='ps' || option=='tg'){
+        document.getElementById("dias").disabled=true;
+        document.getElementById("cantidad").disabled=false;
+    }else{
+        document.getElementById("cantidad").disabled=false;
+        document.getElementById("dias").disabled=false;
+    }
     $.ajax({
         url: 'ver/'+option,
         type: "GET",
         success: function(data){
+            document.getElementById("carga").disabled=false;
            data=JSON.parse(data);
 
             let $select = $('#carga');
             $('#carga').empty();
             for (let index = 0; index < data.length; index++) {
-                console.log( data[index]);
+                //console.log( data[index]);
                 $select.append('<option value=' + data[index].id + '>' + data[index].nombre_carga+
                 '</option>');
                 
@@ -18,7 +29,11 @@ $('#A_carga').on('change', function() {
 
         }, 
         error: function(){
-              alert("No hay datos");
+            document.getElementById("carga").disabled=true;
+            let $select = $('#carga');
+            $('#carga').empty();
+                $select.append('<option value="" selected>No hay datos</option>');
+                
          }
 
         }); 
