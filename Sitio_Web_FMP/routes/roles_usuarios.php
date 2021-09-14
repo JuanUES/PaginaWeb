f122de1791ae5d2fb7b5c0e6e9e31d0a834d0294
Carlos-trj809 <79940098+Carlos-trj809@users.noreply.github.com>
2021-09-14 14:52:42 -0600

    commit reparado de categoria

--- Sitio_Web_FMP/routes/roles_usuarios.php
+++ Sitio_Web_FMP/routes/roles_usuarios.php
@@ -1,17 +1,17 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolesUsuarios\UsuariosController;
 
-//Route::group(['middleware' => ['role:super-admin']], function () {
+Route::group(['middleware' => ['role:super-admin','auth']], function () {
     
-    /**Rutas get */
-    Route::get('admin/Usuarios',[UsuariosController::class,'index'])->name('usuarios')->middleware(['auth']);
-    Route::get('admin/Usuarios/Usuario/{usuario}',[UsuariosController::class,'usuario'])->middleware(['auth']);
-    Route::get('admin/Usuarios/UsuarioRol/{usuario}',[UsuariosController::class,'usuarioRol'])->middleware(['auth']);
+    /**Rutas Get */
+    Route::get('admin/Usuarios',[UsuariosController::class,'index'])->name('usuarios');
+    Route::get('admin/Usuarios/Usuario/{usuario}',[UsuariosController::class,'usuario']);
+    Route::get('admin/Usuarios/UsuarioRol/{usuario}',[UsuariosController::class,'usuarioRol']);
 
     /**Rutas Post */
-    Route::post('admin/usuarios/guardar', [UsuariosController::class,'store'])
-        ->middleware('auth')->name('guardarUser');
-    Route::post('admin/usuarios/estado', [UsuariosController::class,'estado'])
-        ->middleware('auth')->name('usuarioEstado');
-//});
+    Route::post('admin/usuarios/guardar', [UsuariosController::class,'store'])->name('guardarUser');
+    Route::post('admin/usuarios/estado', [UsuariosController::class,'estado'])->name('usuarioEstado');
+
+});
