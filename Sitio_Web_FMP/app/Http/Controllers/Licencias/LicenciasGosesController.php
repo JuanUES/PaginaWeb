3bbcfa297c7b60c84d8cb73a7bedb05472ceab73
SoporteUti <soporte.tifmp@ues.edu.sv>
2021-10-07 14:59:33 -0600

    controller liciencias con gose

---
+++ Sitio_Web_FMP/app/Http/Controllers/Licencias/LicenciasGosesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Licencias;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LicenciasGosesController extends Controller
+{
+    //
+}
