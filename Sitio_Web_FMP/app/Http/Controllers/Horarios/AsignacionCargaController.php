e61e36da15db57d19d39b0c8196203d5c43b5759
SoporteUti <87437110+SoporteUti@users.noreply.github.com>
2021-10-15 11:42:20 -0600

    trabajando en la asignacion de carga admin

--- Sitio_Web_FMP/app/Http/Controllers/Horarios/AsignacionCargaController.php
+++ Sitio_Web_FMP/app/Http/Controllers/Horarios/AsignacionCargaController.php
@@ -30,7 +30,35 @@ class AsignacionCargaController extends Controller
     
     //vamos a insertar los datos a la base de datos
     public function create(Request $request){
+       // echo dd($request);
+       try{//para verficar que mande algo del combo carga
+       
+
+        $validator = Validator::make($request->all(),[
+            'A_carga'   =>'required'
+        ]);         
+
+        if($validator->fails())
+        {            
+            return response()->json(['error'=>$validator->errors()->all()]);                
+        }
+
+    ///************PARA INGRESAR CARGA ADMIN********* */
+    if($request->A_carga=='ad'){
         try{
+
+            $error = null;
+            $validar = DB::table('asig_admins')
+            ->select('*')
+            ->where([['id_carga','=',$request->carga],
+            ['id_ciclo','=',$request->id_ciclo]]);
+
+            if($validar->exists())
+            {
+                $error = 'Carga Administrativa ya Asignada';
+                return response()->json(['error'=>[$error]]);
+            }
+
 
             $validator = Validator::make($request->all(),[
                 'id_empleado'   =>'required',
@@ -43,7 +71,7 @@ class AsignacionCargaController extends Controller
             {            
                 return response()->json(['error'=>$validator->errors()->all()]);                
             }
-        //echo dd($request);
+          //echo dd($request);
             $asig = $request->_id ==null ? new Asig_admin():Asig_admin::findOrFail($request->_id);
             $asig -> id_empleado   = $request->id_empleado;
             $asig -> id_carga      = $request->carga;
@@ -56,6 +84,58 @@ class AsignacionCargaController extends Controller
         }catch(Exception $e){
             return response()->json(['error'=>$e->getMessage()]);
         }
+    }
+    /***FIN PARA INGRESAR CARGA ADMIN */
+
+    ///************PARA TRABAJO DE GRADO********* */
+    if($request->A_carga=='tg'){
+        try{
+
+            $error = null;
+            $validar = DB::table('asig_admins')
+            ->select('*')
+            ->where([['id_carga','=',$request->carga],
+            ['id_ciclo','=',$request->id_ciclo]]);
+
+            if($validar->exists())
+            {
+                $error = 'Carga Administrativa ya Asignada';
+                return response()->json(['error'=>[$error]]);
+            }
+
+
+            $validator = Validator::make($request->all(),[
+                'id_empleado'   =>'required',
+                'carga'         =>'required',
+                'id_ciclo'      =>'required',
+                'dias'          =>'required'
+            ]);         
+
+            if($validator->fails())
+            {            
+                return response()->json(['error'=>$validator->errors()->all()]);                
+            }
+          //echo dd($request);
+            $asig = $request->_id ==null ? new Asig_admin():Asig_admin::findOrFail($request->_id);
+            $asig -> id_empleado   = $request->id_empleado;
+            $asig -> id_carga      = $request->carga;
+            $asig -> id_ciclo      = $request->id_ciclo;
+            $asig -> dias      = $request->dias;
+            $asig -> save();         
+        
+            return $request->_id != null?response()->json(['mensaje'=>'Modificación exitosa.']):response()->json(['mensaje'=>'Registro exitoso.']);
+        
+        }catch(Exception $e){
+            return response()->json(['error'=>$e->getMessage()]);
+        }
+    }
+    /***FIN PARA TRABAJO DE GRADO*******/
+
+   }catch(Exception $e){
+    return response()->json(['error'=>$e->getMessage()]);
+    }
+
+
     }//fin create
 
 
--- Sitio_Web_FMP/database/migrations/2021_10_07_151304_create_ajustes_table.php
+++ Sitio_Web_FMP/database/migrations/2021_10_07_151304_create_ajustes_table.php
@@ -14,7 +14,9 @@ class CreateAjustesTable extends Migration
     public function up()
     {
         Schema::dropIfExists('fecha_uso_licencias');
-        Schema::dropIfExists('permisos'); 
+        Schema::dropIfExists('permisos');
+        Schema::dropIfExists('proyectosociales');
+        Schema::dropIfExists('trabajogrados');
        
         Schema::create('permisos', function (Blueprint $table) {
             $table->id();
@@ -36,6 +38,56 @@ class CreateAjustesTable extends Migration
             $table->foreign('jefatura')->references('id')->on('empleado');
             $table->foreign('gestor_rrhh')->references('id')->on('empleado');
             $table->foreign('empleado')->references('id')->on('empleado');    
+
+            $table->timestamps();
+        });
+
+        Schema::create('proyectosociales', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_empleado');
+            $table->bigInteger('id_carga');
+            $table->bigInteger('id_ciclo');
+            $table->integer('cantidad');
+
+            $table->foreign('id_carga')
+            ->references('id')
+            ->on('carga_admins')
+            ->onDelete('cascade');
+
+            $table->foreign('id_empleado')
+            ->references('id')
+            ->on('empleado')
+            ->onDelete('cascade');
+            
+            $table->foreign('id_ciclo')
+            ->references('id')
+            ->on('ciclos')
+            ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::create('trabajogrados', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_empleado');
+            $table->bigInteger('id_carga');
+            $table->bigInteger('id_ciclo');
+            $table->integer('cantidad');
+
+            $table->foreign('id_empleado')
+            ->references('id')
+            ->on('empleado')
+            ->onDelete('cascade');
+
+            $table->foreign('id_carga')
+            ->references('id')
+            ->on('carga_admins')
+            ->onDelete('cascade');
+
+            $table->foreign('id_ciclo')
+            ->references('id')
+            ->on('ciclos')
+            ->onDelete('cascade');
 
             $table->timestamps();
         });
