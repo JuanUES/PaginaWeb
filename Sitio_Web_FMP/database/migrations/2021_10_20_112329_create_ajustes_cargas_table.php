75a7402d0b67f40629392a70d12acb0662b1b7ab
SoporteUti <87437110+SoporteUti@users.noreply.github.com>
2021-10-20 11:24:12 -0600

    pequeño error

--- Sitio_Web_FMP/database/migrations/2021_10_20_112329_create_ajustes_cargas_table.php
+++ Sitio_Web_FMP/database/migrations/2021_10_20_112329_create_ajustes_cargas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAjustesCargaTable extends Migration
+class CreateAjustesCargasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class CreateAjustesCargaTable extends Migration
      */
     public function up()
     {
-        
+        Schema::dropIfExists('proyectosociales');
+        Schema::dropIfExists('trabajogrados');
         Schema::dropIfExists('asig_admins');
         Schema::create('asig_admins', function (Blueprint $table) {
             $table->id();
@@ -48,6 +49,5 @@ class CreateAjustesCargaTable extends Migration
      */
     public function down()
     {
-      
     }
 }
