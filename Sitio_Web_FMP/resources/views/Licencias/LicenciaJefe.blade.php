cde1fe4cc6512285401f886e0b65a5e00c2c9f49
Carlos-trj809 <46167286+silk0@users.noreply.github.com>
2021-10-28 01:47:05 -0600

    Licencias - Jefatura

--- Sitio_Web_FMP/resources/views/Licencias/LicenciaJefe.blade.php
+++ Sitio_Web_FMP/resources/views/Licencias/LicenciaJefe.blade.php
@@ -1,5 +1,225 @@
 @extends('layouts.admin')
 
+@section('content')
+@if (!is_null(auth()->user()->empleado))
+<
+<!--modal para dar alta-->
+<div id="modalCancelar" class="modal fade bs-example-modal-center" tabindex="-1" role="dialog" 
+    aria-labelledby="myCenterModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="myCenterModalLabel">
+                    <i class="fa fa-ban mdi-24px" style="margin: 0px;"></i> Cancelar</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>
+            <form action="{{ route('lic/cancelar') }}" method="POST" id="cancelarModal">
+                @csrf
+                <div class="modal-body">
+                    <div class="alert alert-primary alert-dismissible bg-primary text-white border-0 fade show"
+                        role="alert" style="display:none" id="notificacion1">
+                    </div>
+                    <input type="hidden" name="_id" id="cancelar_id">
+                    <div class="row py-3">
+                        <div class="col-xl-2 fa fa-exclamation-triangle text-warning fa-4x mr-1"></div>
+                        <div class="col-xl-9 text-black"> 
+                            <h4 class="font-17 text-justify font-weight-bold">
+                                Advertencia: Se cancelara esta licencia, ¿Desea continuar?
+                            </h4>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-xl-6 p-1">
+                            <button  type="submit" 
+                                class="btn p-1 btn-light waves-effect waves-light btn-block font-24">
+                                <i class="mdi mdi-check mdi-16px"></i>
+                                Si
+                            </button>
+                        </div>
+                        <div class="col-xl-6 p-1">
+                            <button type="reset" 
+                                class="btn btn-light p-1 waves-light waves-effect btn-block font-24" 
+                                data-dismiss="modal" >
+                                <i class="mdi mdi-block-helper mdi-16px" aria-hidden="true"></i>
+                                No
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal-->
+<!--Modal para dar alta fin-->
+
+<div id="modalObservaciones" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" 
+    aria-labelledby="myCenterModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="myCenterModalLabel">
+                    <i class="fa fa-eye mdi-36px" style="margin: 0px;"></i> Observaciones</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>           
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <table style="width: 100%" class="table" id="obs-table"> 
+                        <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Procedimiento</th>
+                                <th>Observaciones</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div>
+
+<!-- start page title -->
+<div class="row">
+    <div class="col-xl-12">
+        <div class="page-title-box">
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Inicio</a></li>
+                    <li class="breadcrumb-item active">Jefe</li>
+                </ol>
+            </div>
+            <h4 class="page-title">&nbsp;</h4>
+        </div>
+    </div>
+</div>
+<!-- end page title -->
+
+<div class="row">
+    <div class="col-12">
+        <div class="card-box">
+            <div class="row py-2">
+                <div class="col order-first">
+                    <h3>
+                        Licencias de Empleados para Jefe
+                    </h3>
+                </div>
+                <div class="col-lg-1 order-last">
+                </div>                
+            </div>
+            <table  class="table" style="width: 100%">
+                <thead>
+                <tr>
+                    <th class="col-sm-2">Presentación</th>
+                    <th class="col-sm-2">Uso</th>
+                    <th class="col-sm-1">Tipo</th>
+                    <th class="col-xs-1">Hora Inicio</th>
+                    <th class="col-xs-1">Hora Final</th>
+                    <th class="col-xs-2">Horas</th>
+                    <th class="col-sm-1 text-center">Estado</th>
+                    <th class="col-sm-1 text-center">Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                    
+                    @foreach ($permisos as $item)
+                        <tr>
+                            <th class="align-middle ">{{Carbon\Carbon::parse($item->fecha_presentacion)->format('d/M/Y')}}</th>
+                            <td class="align-middle ">{{Carbon\Carbon::parse($item->fecha_uso)->format('d/M/Y')}}</td>
+                            <td class="align-middle "><span class="badge badge-primary">{{$item->tipo_permiso}}</span></td>
+                            <td class="align-middle ">{{date('H:i', strtotime($item->hora_inicio))}}</td>
+                            <td class="align-middle ">{{date('H:i', strtotime($item->hora_final))}}</td>
+                            <td class="align-middle ">
+                                {{
+                                    Carbon\Carbon::parse($item->fecha_uso.'T'.$item->hora_inicio)
+                                        ->diffAsCarbonInterval(Carbon\Carbon::parse($item->fecha_uso.'T'.$item->hora_final))
+                                }}
+                            </td>
+                            <td class="align-middle text-center">
+                                @if($item->estado =='GUARDADO') 
+                                    <span class="badge badge-primary">{{$item->estado}}</span>
+                                @endif
+                                @if($item->estado =='CANCELADO') 
+                                    <span class="badge badge-danger">{{$item->estado}}</span>
+                                @endif
+                                @if ($item->estado =='ENVIADO A JEFATURA')
+                                    <span class="badge badge-warning">{{$item->estado}}</span>
+                                @endif
+                            </td>
+                            <td class="align-middle ">
+                                <div class="row">
+                                    <div class="col text-center">
+                                        @php
+                                            $todos_btn = $item->estado =='GUARDADO' or 
+                                                        !$item->estado=='ENVIADO A JEFATURA';
+                                        @endphp
+                                        <div class="btn-group" role="group">
+                                            <button title="Observaciones" class="btn btn-outline-primary btn-sm rounded-left" 
+                                            @if ($item->estado =='CANCELADO')
+                                                disabled
+                                            @else
+                                              value="{{$item->permiso}}" 
+                                              onclick="observaciones(this)"
+                                            @endif>
+                                                <i class="fa fa-eye font-16 my-1" aria-hidden="true"></i>
+                                            </button>
+                                            <button title="Enviar" class="btn btn-outline-primary btn-sm" 
+                                            @if($todos_btn)
+                                                value="{{$item->permiso}}"
+                                                 onclick="enviar(this)"
+                                                 @else disabled
+                                                 @endif>                                                
+                                                <i class="fa fa-arrow-circle-up font-16 my-1" aria-hidden="true"></i>
+                                            </button>
+                                            <button title="Editar" 
+                                            class="btn btn-outline-primary btn-sm border-letf-0"  
+                                            @if($todos_btn)
+                                                 value="{{$item->permiso}}"
+                                                onclick="editar(this)"
+                                                @else
+                                                disabled
+                                                @endif>
+                                                <i class="fa fa-edit font-16 my-1" aria-hidden="true"></i>
+                                            </button>
+                                            <button title="Cancelar" 
+                                                class="btn btn-outline-primary btn-sm border-left-0 btn-outline-danger rounded-right"
+                                                @if($todos_btn)
+                                                 onclick="cancelar(this)"
+                                                 value="{{$item->permiso}}"
+                                                @else
+                                                disabled
+                                                @endif>
+                                                <i class="fa fa-ban font-16 my-1"></i>
+                                            </button>                                   
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>  
+                        </tr>
+                    @endforeach                   
+                </tbody>
+            </table>
+
+        </div> <!-- end card-box -->
+    </div> <!-- end col -->
+</div>
+<!-- end row -->
+
+@else
+    <div class="row m-3">
+        <div class="col-xl-12">
+            <div class="card-box p-2 border">
+                <p> <i class="fa fa-info-circle"></i> No es posible cargar la información perteneciente a <strong> {{auth()->user()->name}} </strong>.</p>
+                <label> A continuación se detallan las posibles causas: </label>
+                <ul>
+                    <li>El Usuario no se encuentra vinculado con ningun <strong>Empleado</strong> registrado en el sistema.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endif
+@endsection
+
 @section('plugins')
 <link href="{{ asset('template-admin/dist/assets/libs/select2/select2.min.css') }}" rel="stylesheet"/>
 <link href="{{ asset('template-admin/dist/assets/libs/bootstrap-select/bootstrap-select.min.css') }}" rel="stylesheet"/>
