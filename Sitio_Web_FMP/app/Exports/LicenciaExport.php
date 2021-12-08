a7457a696dfc09ae675e771c6a39a74b8cd50efe
JuanTr3j0 <juan-carlos-trejo@hotmail.com>
2021-12-07 23:08:45 -0600

    Reestructuracion de la consulta para el archivo exel

--- Sitio_Web_FMP/app/Exports/LicenciaExport.php
+++ Sitio_Web_FMP/app/Exports/LicenciaExport.php
@@ -10,7 +10,6 @@ class LicenciaExport implements FromView{
     public $tipo_contrato;
     public $anio;
     public $mes;
-    public $mes_ant;
     public $departamento;
     public $comentario;
 
@@ -20,7 +19,6 @@ class LicenciaExport implements FromView{
 
         //$this->tipo_contrato  = $tipo_contrato;
         $this->departamento = $departamento;
-        $this->mes_ant = ($this->mes>1?$this->mes-1:$this->mes);
         $this->comentario = $comentario;
     }
 
@@ -29,72 +27,85 @@ class LicenciaExport implements FromView{
     */
 
     public function view(): View{
-        $query = "
-        select TRIM(e.apellido)||','||TRIM(e.nombre) as nombre,
+        $query = "select TRIM(e.apellido)||','||TRIM(e.nombre) as nombre,
         null as hrs_inpunt,null as hrs_inasis,null as hrs_descont,
-        to_char(sum(p1.hora_final-p1.hora_inicio),'HH24:MI') hrs_lc_gs,
-        to_char(sum(p2.hora_final-p2.hora_inicio),'HH24:MI') hrs_ls_gs,
-        to_char(sum(p3.hora_final-p3.hora_inicio),'HH24:MI') hrs_l_ofic,
-        to_char(sum(p4.hora_final-p4.hora_inicio),'HH24:MI') hrs_t_comp,
-        to_char(sum(p5.hora_final-p5.hora_inicio),'HH24:MI') hrs_incap,
-        to_char(sum(p6.hora_final-p6.hora_inicio),'HH24:MI') hrs_lc_gs_jd,
-        count(p7.*) cant_olvido,
-        to_char(sum(p8.hora_final-p8.hora_inicio),'HH24:MI') hrs_paternidad_duelo,       
-            CASE WHEN (".$this->mes.">1) THEN
-            (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_ant from empleado e1
-                inner join permisos on empleado = e1.id
-                    and permisos.estado like 'Aceptado'
-                    and tipo_permiso like 'LC/GS'
-                    and to_char(fecha_uso,'YYYY')::int=".$this->anio."
-                    and to_char(fecha_uso,'MM')::int=".($this->mes_ant)." where e1.id=e.id)
-                ELSE 
-                (select '00:00' hrs_lc_gs_ant)
-            END,            
-        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_acu from empleado e2
-                inner join permisos  on empleado = e2.id and permisos.estado like 'Aceptado' and
-                tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=".$this->anio." and to_char(fecha_uso,'MM')::int<=".$this->mes." where  e.id = e2.id
-        ),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs
+        from permisos  where empleado = e.id and estado like '%Aceptado%'
+        and tipo_permiso like '%LC/GS%'
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_ls_gs
+        from permisos where empleado = e.id and estado like '%Aceptado%' and tipo_permiso like '%LS/GS%'
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_l_ofic from permisos
+        where empleado = e.id and estado like '%Aceptado%' and
+        (tipo_permiso like '%L.OFICIAL/A%' or tipo_permiso like '%L OFICIAL%')
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_t_comp
+        from permisos where empleado = e.id and estado like '%Aceptado%' and tipo_permiso like '%T COMP%'
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_incap
+        from permisos where empleado = e.id and estado like '%Aceptado%' and
+        (tipo_permiso like '%INCAP%' or tipo_permiso like '%INCAPACIDAD/A%')
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_jd
+        from permisos where empleado = e.id and estado like '%Aceptado%' and
+        (tipo_permiso like '%FUMIGACIÓN%' or tipo_permiso like '%ESTUDIO%' or tipo_permiso like '%OTROS%')
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select count(*) cant_olvido
+        from permisos where empleado = e.id and estado like '%Aceptado%' and
+        tipo_permiso like '%Const. olvido%'
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_paternidad_duelo
+        from permisos where empleado = e.id and estado like '%Aceptado%' and
+        tipo_permiso like '%DUELO O PATERNIDAD%'
+        and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+        and to_char(fecha_uso,'MM')::int=". $this->mes ."),
+        
+        CASE WHEN (". $this->mes .">1) THEN
+           (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_ant
+           from permisos where empleado = e.id
+                  and permisos.estado like '%Aceptado%'
+                  and tipo_permiso like '%LC/GS%'
+                  and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+                  and to_char(fecha_uso,'MM')::int=(". $this->mes ."-1))
+            ELSE
+            (select '00:00' hrs_lc_gs_ant)
+        END,
+        
+        (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_acu
+            from permisos  where empleado = e.id  and permisos.estado like '%Aceptado%' and
+            tipo_permiso like '%LC/GS%' and to_char(fecha_uso,'YYYY')::int=". $this->anio ." and
+            to_char(fecha_uso,'MM')::int<=". $this->mes ."),
+        
         lcg.anuales||':00' hrs_lc_gs_anuales,
-        (select lcg.anuales-to_char(sum(hora_final-hora_inicio),'HH24')::int||':'||to_char(sum(hora_final-hora_inicio),'MI')
-           hrs_disp from empleado e3
-           inner join permisos on empleado = e3.id and permisos.estado like 'Aceptado' and
-        tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=".$this->anio." where  e3.id = e.id)
+        
+        (select (lcg.anuales-to_char(sum(hora_final-hora_inicio),'HH24')::int)||':'||to_char(sum(hora_final-hora_inicio),'MI')
+            hrs_disp from permisos where empleado = e.id and permisos.estado like 'Aceptado' and
+        tipo_permiso like '%LC/GS%' and to_char(fecha_uso,'YYYY')::int=". $this->anio ." group by lcg.anuales)
+        
         from empleado e
-
-        left join permisos p1 on p1.empleado = e.id and p1.estado like 'Aceptado' and p1.tipo_permiso like 'LC/GS'
-        and to_char(p1.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p1.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p2 on p2.empleado = e.id and p2.estado like 'Aceptado' and p2.tipo_permiso like 'LS/GS'
-        and to_char(p2.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p2.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p3 on p3.empleado = e.id and p3.estado like 'Aceptado' and
-        (p3.tipo_permiso like 'L.OFICIAL/A' or p3.tipo_permiso like 'L OFICIAL')
-        and to_char(p3.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p3.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p4 on p4.empleado = e.id and p4.estado like 'Aceptado' and p4.tipo_permiso like 'T COMP'
-        and to_char(p4.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p4.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p5 on p5.empleado = e.id and p5.estado like 'Aceptado' and
-        (p5.tipo_permiso like 'INCAP' or p5.tipo_permiso like 'INCAPACIDAD/A')
-        and to_char(p5.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p5.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p6 on p6.empleado = e.id and p6.estado like 'Aceptado' and
-        (p6.tipo_permiso like 'FUMIGACIÓN' or p6.tipo_permiso like 'ESTUDIO' or p6.tipo_permiso like 'OTROS')
-        and to_char(p6.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p6.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p7 on p7.empleado = e.id and p7.estado like 'Aceptado' and
-        p7.tipo_permiso like 'Const. olvido' and to_char(p7.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p7.fecha_uso,'MM')::int=".$this->mes."
-
-        left join permisos p8 on p8.empleado = e.id and p8.estado like 'Aceptado' and
-        p8.tipo_permiso like 'DUELO O PATERNIDAD' and to_char(p8.fecha_uso,'YYYY')::int=".$this->anio." and to_char(p8.fecha_uso,'MM')::int=".$this->mes."
-
+        
         inner join tipo_jornada tj on e.id_tipo_jornada = tj.id
         inner join licencia_con_goses lcg on tj.id = lcg.id_tipo_jornada
-        
-        where e.id_depto=".$this->departamento."
-
+        where e.id_depto=". $this->departamento ."
         group by e.nombre,e.apellido, lcg.anuales,e.id
-        order by e.apellido,e.nombre asc;";
+        order by e.apellido asc;
+        ";
 
         $query=trim($query);
 
@@ -119,63 +130,84 @@ class LicenciaExport implements FromView{
     }
 
     /**CONSULTA PURA*/
-    /*select TRIM(e.apellido)||','||TRIM(e.nombre) as nombre,null as hrs_inpunt,null as hrs_inasis,null as hrs_descont,
-       to_char(sum(p1.hora_final-p1.hora_inicio),'\'HH24:MI'\') hrs_lc_gs,
-       to_char(sum(p2.hora_final-p2.hora_inicio),'\'HH24:MI'\') hrs_ls_gs,
-       to_char(sum(p3.hora_final-p3.hora_inicio),'\'HH24:MI'\') hrs_l_ofic,
-       to_char(sum(p4.hora_final-p4.hora_inicio),'\'HH24:MI'\') hrs_t_comp,
-       to_char(sum(p5.hora_final-p5.hora_inicio),'\'HH24:MI'\') hrs_incap,
-       to_char(sum(p6.hora_final-p6.hora_inicio),'\'HH24:MI'\') hrs_lc_gs_jd,
-       count(p7.*) cant_olvido,
-       to_char(sum(p8.hora_final-p8.hora_inicio),'\'HH24:MI'\') hrs_paternidad_duelo,
+    /*select TRIM(e.apellido)||','||TRIM(e.nombre) as nombre,
+null as hrs_inpunt,null as hrs_inasis,null as hrs_descont,
 
-       (select to_char(sum(hora_final-hora_inicio),'\'HH24:MI'\') hrs_lc_gs_ant from empleado e1
-           inner join permisos on empleado = e1.id and permisos.estado like 'Aceptado' and
-       tipo_permiso like 'LC/GS' and to_char(fecha_uso,'MM-YYYY') like '2021-10' where e1.id=e.id),
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs
+from permisos  where empleado = e.id and estado like '%Aceptado%'
+and tipo_permiso like '%LC/GS%'
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-       (select to_char(sum(hora_final-hora_inicio),'\'HH24:MI'\') hrs_lc_gs_acu from empleado e2
-           inner join permisos  on empleado = e2.id and permisos.estado like 'Aceptado' and
-        tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=2021 where  e.id = e2.id),
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_ls_gs
+from permisos where empleado = e.id and estado like '%Aceptado%' and tipo_permiso like '%LS/GS%'
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-       lcg.anuales||':00' hrs_lc_gs_anuales,
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_l_ofic from permisos
+where empleado = e.id and estado like '%Aceptado%' and
+(tipo_permiso like '%L.OFICIAL/A%' or tipo_permiso like 'L OFICIAL')
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-       (select lcg.anuales-to_char(sum(hora_final-hora_inicio),'HH24')::int||':'||to_char(sum(hora_final-hora_inicio),'MI')
-           hrs_lc_gs_disp from empleado e3
-           inner join permisos on empleado = e3.id and permisos.estado like 'Aceptado' and
-        tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=2021 where  e3.id = e.id)
-        from empleado e
-        left join permisos p1 on p1.empleado = e.id and p1.estado like 'Aceptado' and p1.tipo_permiso like 'LC/GS'
-        and to_char(p1.fecha_uso,'YYYY')::int=2021 and to_char(p1.fecha_uso,'MM')::int=11
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_t_comp
+from permisos where empleado = e.id and estado like '%Aceptado%' and tipo_permiso like '%T COMP%'
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-        left join permisos p2 on p2.empleado = e.id and p2.estado like 'Aceptado' and p2.tipo_permiso like 'LS/GS'
-        and to_char(p2.fecha_uso,'YYYY')::int=2021 and to_char(p2.fecha_uso,'MM')::int=11
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_incap
+from permisos where empleado = e.id and estado like '%Aceptado%' and
+(tipo_permiso like '%INCAP%' or tipo_permiso like '%INCAPACIDAD/A%')
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-        left join permisos p3 on p3.empleado = e.id and p3.estado like 'Aceptado' and
-        (p3.tipo_permiso like 'L.OFICIAL/A' or p3.tipo_permiso like 'L OFICIAL')
-        and to_char(p3.fecha_uso,'YYYY')::int=2021 and to_char(p3.fecha_uso,'MM')::int=11
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_jd
+from permisos where empleado = e.id and estado like '%Aceptado%' and
+(tipo_permiso like '%FUMIGACIÓN%' or tipo_permiso like '%ESTUDIO%' or tipo_permiso like '%OTROS%')
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-        left join permisos p4 on p4.empleado = e.id and p4.estado like 'Aceptado' and p4.tipo_permiso like 'T COMP'
-        and to_char(p4.fecha_uso,'YYYY')::int=2021 and to_char(p4.fecha_uso,'MM')::int=11
+(select count(*) cant_olvido
+from permisos where empleado = e.id and estado like '%Aceptado%' and
+tipo_permiso like '%Const. olvido%'
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-        left join permisos p5 on p5.empleado = e.id and p5.estado like 'Aceptado' and
-        (p5.tipo_permiso like 'INCAP' or p5.tipo_permiso like 'INCAPACIDAD/A')
-        and to_char(p5.fecha_uso,'YYYY')::int=2021 and to_char(p5.fecha_uso,'MM')::int=11
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_paternidad_duelo
+from permisos where empleado = e.id and estado like 'Aceptado' and
+tipo_permiso like 'DUELO O PATERNIDAD'
+and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+and to_char(fecha_uso,'MM')::int=". $this->mes ."),
 
-        left join permisos p6 on p6.empleado = e.id and p6.estado like 'Aceptado' and
-        (p6.tipo_permiso like 'FUMIGACIÓN' or p6.tipo_permiso like 'ESTUDIO' or p6.tipo_permiso like 'OTROS')
-        and to_char(p6.fecha_uso,'YYYY')::int=2021 and to_char(p6.fecha_uso,'MM')::int=11
+CASE WHEN (". $this->mes .">1) THEN
+   (select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_ant
+   from permisos where empleado = e.id
+          and permisos.estado like 'Aceptado'
+          and tipo_permiso like 'LC/GS'
+          and to_char(fecha_uso,'YYYY')::int=". $this->anio ."
+          and to_char(fecha_uso,'MM')::int=(". $this->mes ."-1))
+    ELSE
+    (select '00:00' hrs_lc_gs_ant)
+END,
 
-        left join permisos p7 on p7.empleado = e.id and p7.estado like 'Aceptado' and
-        p7.tipo_permiso like 'Const. olvido' and to_char(p7.fecha_uso,'YYYY')::int=2021 and to_char(p7.fecha_uso,'MM')::int=11
+(select to_char(sum(hora_final-hora_inicio),'HH24:MI') hrs_lc_gs_acu
+    from permisos  where empleado = e.id  and permisos.estado like 'Aceptado' and
+    tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=". $this->anio ." and
+    to_char(fecha_uso,'MM')::int<=". $this->mes ."),
 
-        left join permisos p8 on p8.empleado = e.id and p8.estado like 'Aceptado' and
-        p8.tipo_permiso like 'DUELO O PATERNIDAD' and to_char(p8.fecha_uso,'YYYY')::int=2021 and to_char(p8.fecha_uso,'MM')::int=11
+lcg.anuales||':00' hrs_lc_gs_anuales,
 
-        inner join tipo_jornada tj on e.id_tipo_jornada = tj.id
-        inner join licencia_con_goses lcg on tj.id = lcg.id_tipo_jornada
-        where e.id_tipo_contrato=1 and e.id_depto=1
+(select (lcg.anuales-to_char(sum(hora_final-hora_inicio),'HH24')::int)||':'||to_char(sum(hora_final-hora_inicio),'MI')
+   hrs_lc_gs_disp from permisos where empleado = e.id and permisos.estado like 'Aceptado' and
+tipo_permiso like 'LC/GS' and to_char(fecha_uso,'YYYY')::int=". $this->anio ." group by lcg.anuales)
 
-        group by e.nombre,e.apellido, lcg.anuales,e.id
-        order by e.apellido,e.nombre asc;
+from empleado e
+
+inner join tipo_jornada tj on e.id_tipo_jornada = tj.id
+inner join licencia_con_goses lcg on tj.id = lcg.id_tipo_jornada
+where e.id_depto=5
+group by e.nombre,e.apellido, lcg.anuales,e.id
+order by e.apellido asc;
+
      */
 }
